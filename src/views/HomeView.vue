fb16fe337db32f9a8c61f67d8e84342470dc12e6
Maximilian Vorwerk <maximilian.vorwerk@student.htw-berlin.de>
2023-06-14 19:17:21 +0200

    Milestone 4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,82 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
-
 <template>
   <main>
     <TheWelcome />
   </main>
 </template>
+
+<script setup>
+  import TheWelcome from '../components/TheWelcome.vue';
+
+  export default {
+    name: 'HomeView',
+
+    data() {
+      return {
+        inventoryObjects: []
+      }
+    },
+
+    methods: {
+
+      getInventoryObject () {
+        const endpoint = 'http://localhost:8080/inventoryObject/{id}'
+        const requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        }
+
+        fetch(endpoint, requestOptions)
+            .then(response => response.json())
+            .then(result => result.forEach(inventoryObject => this.inventoryObjects.push(inventoryObject)))
+            .catch(error => console.log('error', error))
+      },
+
+      createInventoryObject () {
+        const endpoint = 'http://localhost:8080/inventoryObject/{id}'
+        const data = {
+          inventoryObjects: this.getInventoryObject()
+        }
+        const requestOptions = {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(data)
+        }
+
+        fetch(endpoint, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log('Success', data))
+            .catch(error => console.log('error', error))
+      },
+
+      updateInventoryObject () {
+        const endpoint = 'http://localhost:8080/inventoryObject/{id}'
+        const data = {
+          inventoryObjects: this.getInventoryObject()
+        }
+        const requestOptions = {
+          method: 'PUT',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(data)
+        }
+
+        fetch(endpoint, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log('Success', data))
+            .catch(error => console.log('error', error))
+      },
+
+      deleteInventoryObject () {
+        const endpoint = 'http://localhost:8080/inventoryObject/{id}'
+        const requestOptions = {
+          method: 'DELETE',
+          headers: {'Content-Type': 'application/json'},
+        }
+
+        fetch(endpoint, requestOptions)
+            .then(response => response.json())
+            .then(data => console.log('Success', data))
+            .catch(error => console.log('error', error))
+      }
+    }
+  }
+</script>
