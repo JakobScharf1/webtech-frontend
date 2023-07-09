1bae079d92acf7eca8214e6ebf60d6e52e75fa1d
CI <CI@example.com>
2023-07-09 17:28:31 +0000

    deploy

---
+++ assets/AboutView-9819a225.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as _}from"./index-6bf191c8.js";const s={},c={class:"about"},n=_("h1",null,"Eventuell hier erweiterbar",-1),a=[n];function r(i,l){return t(),o("div",c,a)}const d=e(s,[["render",r]]);export{d as default};
